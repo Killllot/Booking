cc31fd190f8ab132e3d7c952731be864f49dee41
Кирилл Филатов <fillontino@gmail.com>
2022-05-13 23:50:14 +0300

    Подготовка к тестам

---
+++ src/test/java/com/newbooking/testCont/PostgresTestContainer.java
@@ -0,0 +1,10 @@
+package com.newbooking.testCont;
+
+import lombok.experimental.UtilityClass;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@UtilityClass
+public class PostgresTestContainer  {
+
+
+}
