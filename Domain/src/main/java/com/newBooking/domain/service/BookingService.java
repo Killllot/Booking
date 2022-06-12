4ddc917e5fc64405fd718ca8ea3a309583c357df
Кирилл Филатов <fillontino@gmail.com>
2022-06-12 21:31:52 +0300

    Сделано: вывод постранично и с сортировкой. Пытаюсь собрать нормальный jar файл, но он выдает ошибку при его запуске

--- Domain/src/main/java/com/newBooking/domain/service/BookingService.java
+++ Domain/src/main/java/com/newBooking/domain/service/BookingService.java
@@ -10,11 +10,14 @@ import com.newBooking.domain.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -66,26 +69,34 @@ public class BookingService {
         return booking;
     }
 
+    private Sort.Direction getSortDirection(String direction) {
+        if (direction.equals("asc")) {
+            return Sort.Direction.ASC;
+        } else if (direction.equals("desc")) {
+            return Sort.Direction.DESC;
+        }
 
-    /*public List<BookingEntity> getBookingByPage(Long quantity, Long page) {
-        Long first = 1+(page-1)*quantity;
-        Long last = page*quantity;
-        Long count = bookingRepository.getCountBookings();
-        List<BookingEntity> bookings = bookingRepository.findBookingEntitiesByPage(first, last).orElse(null);
-
-        return bookings;
-    }*/
-
-    public Page<BookingEntity> getBookingByPage(Pageable pageable) {
-        Page<BookingEntity> bookings = bookingRepository.findAll(pageable);
-
-        return bookings;
+        return Sort.Direction.ASC;
     }
 
-    public Page<BookingEntity> getBookingByPageWithSorting(Pageable pageable) {
-        Page<BookingEntity> bookings = bookingRepository.findAll(pageable);
+    public Page<BookingEntity> getBookingByPage(Integer quantity, Integer page,String[] sort) {
+        List<Sort.Order> orders = new ArrayList<Sort.Order>();
 
-        return bookings;
+        if (sort[0].contains(",")) {
+            // will sort more than 2 fields
+            // sortOrder="field, direction"
+            for (String sortOrder : sort) {
+                String[] _sort = sortOrder.split(",");
+                orders.add(new Sort.Order(getSortDirection(_sort[1]), _sort[0]));
+            }
+        } else {
+            // sort=[field, direction]
+            orders.add(new Sort.Order(getSortDirection(sort[1]), sort[0]));
+        }
+
+        Pageable paging = PageRequest.of(page,quantity,Sort.by(orders));
+
+        return bookingRepository.findAll(paging);
     }
 
     public List<BookingEntity> getAll() {
--- mainApp/src/main/java/com/newBooking/controllers/BookingController.java
+++ mainApp/src/main/java/com/newBooking/controllers/BookingController.java
@@ -30,16 +30,6 @@ public class BookingController {
     @Autowired
     private BookingService bookingService;
 
-    private Sort.Direction getSortDirection(String direction) {
-        if (direction.equals("asc")) {
-            return Sort.Direction.ASC;
-        } else if (direction.equals("desc")) {
-            return Sort.Direction.DESC;
-        }
-
-        return Sort.Direction.ASC;
-    }
-
     @PostMapping("/create")
     public ResponseEntity createBooking(@Valid  @RequestBody ValidatedBookingDto book) {
             return ResponseEntity
@@ -64,22 +54,7 @@ public class BookingController {
                                                @RequestParam(defaultValue = "0") Integer page,
                                                @RequestParam(defaultValue = "id,desc") String[] sort) {
 
-        List<Sort.Order> orders = new ArrayList<Sort.Order>();
-
-        if (sort[0].contains(",")) {
-            // will sort more than 2 fields
-            // sortOrder="field, direction"
-            for (String sortOrder : sort) {
-                String[] _sort = sortOrder.split(",");
-                orders.add(new Sort.Order(getSortDirection(_sort[1]), _sort[0]));
-            }
-        } else {
-            // sort=[field, direction]
-            orders.add(new Sort.Order(getSortDirection(sort[1]), sort[0]));
-        }
-
-        Pageable paging = PageRequest.of(page,quantity,Sort.by(orders));
-        return new ResponseEntity<>(bookingService.getBookingByPage(paging).stream()
+        return new ResponseEntity<>(bookingService.getBookingByPage(quantity,page,sort).stream()
                 .map(BookingMapper::toModel)
                 .collect(Collectors.toList()), HttpStatus.FOUND);
     }
